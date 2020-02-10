fcb7ba2cb572e04a8712b7001be4b4547926a8cd
Mark <mlordi80@hotmail.com>
2020-02-10 10:27:53 -0500

    Implement many to many relationship and make the favorite button work

--- app/Question.php
+++ app/Question.php
@@ -9,11 +9,13 @@ class Question extends Model
 {
     protected $fillable = ['title', 'body'];
     
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function setTitleAttribute($value) {
+    public function setTitleAttribute($value)
+    {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
@@ -47,12 +49,31 @@ class Question extends Model
     public function answers()
     {
         return $this->hasMany(Answer::class);
-
     }
 
     public function acceptBestAnswer(Answer $answer)
     {
         $this->best_answer_id = $answer->id;
         $this->save();
+    }
+
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+    }
+
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
     }
 }
