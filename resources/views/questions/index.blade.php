d8382740ff6c989d609e9408da60d56bc1b9bd8e
Mark <mlordi80@hotmail.com>
2020-02-10 13:24:33 -0500

    Implement many to many polymorphic relationship to make the vote question controls working

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -21,7 +21,7 @@
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
-                                    <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->vote) }}
+                                    <strong>{{ $question->votes_count }}</strong> {{ Str::plural('vote', $question->vote) }}
                                 </div>
                                 <div class="status {{ $question->status }}">
                                     <strong>{{ $question->answers_count }}</strong> {{ Str::plural('answer', $question->answers_count) }}
